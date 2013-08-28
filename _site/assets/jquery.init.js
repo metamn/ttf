027cd2acda66729d7ec09835e124c0458d862d21
metamn <bartus.csongor@gmail.com>
2013-08-28 09:11:20 +0300

    Variants extended to separate slides

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
 
-  // Close timeline
-  $('section:nth-child(2) article aside dl dd span').click(function() {
-    $(this).parent().hide('slow');
-  });
+  
 
   // Toggle dl's
   $('section article aside dl dt').click(function() {
     $(this).nextUntil('dt').slideToggle();
   });
 
-  // Scroll to Variant, first slide
-  $('section:nth-child(1) article aside h2').click(function() {
-    scrollTo($('section:nth-child(2)'));
-    
-    var index = $(this).index() + 2;
-    $('section:nth-child(2) article aside dl dt:nth-child(' + index + ')').next().show();
+  
+  // Scroll to variants
+  $('section article header ol li').click(function() {
+    var index = $(this).index() + 3;
+    var slide = $('section:nth-child(' + index + ')');
+    scrollTo(slide);
   });
+  
+  $('section:nth-child(2) article aside p ').click(function() {
+    var index = $(this).index() + 2;
+    var slide = $('section:nth-child(' + index + ')');
+    scrollTo(slide);
+  });
+  
 
 
   // Scroll to Team, frontpage
   $('section:nth-child(1) article header h2 span').click(function() {
-    scrollTo($('section:nth-child(4)'));
+    scrollTo($('section:nth-child(7)'));
   });
   
   
   // Scroll in Footer
   $('#footer nav ul li').click(function() {
-    var index = $(this).index() + 2;
+    var index = $(this).attr('data-id');
     var slide = $('section:nth-child(' + index + ')');
     scrollTo(slide);
   });
